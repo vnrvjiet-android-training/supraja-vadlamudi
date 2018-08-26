470f0eed97503c8fc9bb125652c678d782e3266e
supraja-vadlamudi <supraja1998.vadlamudi@gmail.com>
2018-08-26 20:53:20 +0530

    commit

---
+++ loginform/app/src/main/java/com/example/supraja_pc/loginform/Submitactivity.java
@@ -0,0 +1,17 @@
+package com.example.supraja_pc.loginform;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Submitactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit);
+    }
+}
