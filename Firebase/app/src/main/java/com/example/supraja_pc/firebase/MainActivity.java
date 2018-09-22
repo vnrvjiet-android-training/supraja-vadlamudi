a5c6bc60a65010f4d606c402e6f5ddfd8cdfbb86
supraja-vadlamudi <supraja1998.vadlamudi@gmail.com>
2018-09-22 17:22:22 +0530

    commit

---
+++ Firebase/app/src/main/java/com/example/supraja_pc/firebase/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.supraja_pc.firebase;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        String email;
+        String password;
+        mAuth.createUserWithEmailAndPassword(email="supraja1998.vadlamudi@gmail.com", password="abcdef")
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("log", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("log", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+}
